46f2e60098622e022e51e4fe3ccca4c9c121b920
zetachua <zetachua@users.noreply.github.com>
2020-11-26 21:26:05 +0800

    SystemMgr comments added

--- MySTARS/src/com/mySTARS/Control/SystemMgr.java
+++ MySTARS/src/com/mySTARS/Control/SystemMgr.java
@@ -4,48 +4,52 @@ import java.text.ParseException;
 
 import com.mySTARS.Boundary.*;
 import com.mySTARS.Entities.*;
-
+/**
+ * manages interactions between the boundaries and the system
+ */
 public class SystemMgr {
-
+	/**
+	 * Allows admin to view admin display menu once he / she has logged in successfully
+	 * @param staffAccount admin account required
+	 */
 	public static void enterAdmin(StaffAccount staffAccount) {
-		/**
-		* @param Allows admin to view admin display menu once he / she has logged in successfully
-		*/
 		AdminScreen.enterAdmin(staffAccount);
 	}
-	
+	/**
+	 * Allows student to view student display menu once he / she has logged in successfully
+	 * @param studentAccount student account required
+	 */
 	public static void enterUser(StudentAccount studentAccount) {
-		/**
-		* @param Allows student to view student display menu once he / she has logged in successfully
-		*/
 		UserScreen.enterStudent(studentAccount);
 	}
-	
+	/**
+	 * admin to edit access period via accessing SystemBackend class
+	 * @param startAccess start access period to allow students to enter the system
+	 * @param endAccess end access period to allow students to enter the system
+	 */
 	public static void editAccessPeriod(String startAccess, String endAccess) {
-		/**
-		* @param Allows admin to edit access period via accessing SystemBackend class
-		*/
 		SystemBackend.editAccessPeriod(startAccess, endAccess);
 	}
-
+	/**
+	 * SystemBackend class is accessible
+	 * @return
+	 * @throws ParseException
+	 */
 	public static boolean getAccess() throws ParseException {
-		/**
-		* @param Ensures SystemBackend class is accessible
-		*/
 		return SystemBackend.isAccessible();
 	}
-
+	/**
+	 * Gets access date for MySTARS from SystemBackend class
+	 * @return
+	 */
 	public static String getAccessDate() {
-		/**
-		* @param Gets access date for MySTARS from SystemBackend class
-		*/
 		return SystemBackend.getAccessDate();
 	}
-
+	/**
+	 * Gets course list from SystemBackend class
+	 * @return
+	 */
 	public static String getGeneralCourseList() {
-		/**
-		* @param Gets course list from SystemBackend class
-		*/
 		return SystemBackend.getGeneralCourseList();
 	}
 

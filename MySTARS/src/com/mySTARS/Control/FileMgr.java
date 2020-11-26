864a6969efd20a9a4dea2f9995731355c3305b4a
zetachua <zetachua@users.noreply.github.com>
2020-11-26 21:01:01 +0800

    file mgr comments added

--- MySTARS/src/com/mySTARS/Control/FileMgr.java
+++ MySTARS/src/com/mySTARS/Control/FileMgr.java
@@ -6,21 +6,28 @@ import java.util.Scanner;
 
 import com.mySTARS.Entities.SystemBackend;
 
+/**
+ * File manager for handling of data in external files
+ * 
+ */
 public class FileMgr {
-	
+	/**
+	 * Prints the data stored in the files
+	 * @param fileName name of the file
+	 */
 	public static void printFile(String fileName) {
 		try {
 			Scanner scStream = new Scanner(new File(fileName));
 			String inputLine;
 			System.out.println("The file contains: ");
-			/**
-	    	* @param (scStream.nextLine()) (Used to skip to next line if there is no input in the file)
+			/*
+	    	* (scStream.nextLine()) (Used to skip to next line if there is no input in the file)
 	    	*/
 			if (scStream.hasNext()) {
 				scStream.nextLine();  
 			}
-			/**
-	    	* @param (inputLine) (Whatever that is read in is to be used in the system)
+			/*
+	    	* (inputLine) (Whatever that is read in is to be used in the system)
 	    	*/
 			while(scStream.hasNext()) {
 				inputLine = scStream.nextLine(); 
@@ -36,13 +43,17 @@ public class FileMgr {
 			System.exit(0);
 		}
 	}
-
+	/**
+	 * saves all data into external files
+	 */
 	public static void saveAll() {
 		SerializationMgr.serializeMap((HashMap) SystemBackend.StudentAccountMap, "studentData.mystars");
 		SerializationMgr.serializeMap((HashMap) SystemBackend.StaffAccountMap, "staffData.mystars");
 		SerializationMgr.serializeMap((HashMap) SystemBackend.CourseMap, "courseData.mystars");
 	}
-
+	/**
+	 * loads all data from external files into the system
+	 */
 	public static void readAll() {
 		SystemBackend.StudentAccountMap =	SerializationMgr.deserializeMap("studentData.mystars");
 		SystemBackend.StaffAccountMap 	= 	SerializationMgr.deserializeMap("staffData.mystars");
